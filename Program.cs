0cb8d781ee3cc2fd6ff966ccb749a8a2a96a5c93
catalynsava <catalynsava@gmail.com>
2025-05-06 06:44:05 +0300

    validare an

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine( verificareCNP("1") );
+        Console.WriteLine( verificareCNP("525") );
     }
     private static readonly string[] m = {"1", "3", "5", "7"};
     private static readonly string[] f = ["2", "4", "6", "8"];
@@ -16,18 +16,18 @@ class Program
          //sex valid
         if (mCnp.Length > 0)
         {
-            if (int.TryParse( mCnp, out _ ))
+            if (int.TryParse( mCnp.Substring(0,1), out _ ))
             {
                 if(
                 int.Parse( mCnp.Substring(0,1) ) > 0 
                 && 
                 int.Parse( mCnp.Substring(0,1) ) < 9
                 ) {
-                    if (m.Contains(mCnp))
+                    if (m.Contains(mCnp.Substring(0,1)))
                     {
                         mesajValidare = "M";
                     }
-                    else if ( f.Contains(mCnp) )
+                    else if ( f.Contains(mCnp.Substring(0,1)) )
                     {
                         mesajValidare = "F";
                     }else{
@@ -43,7 +43,32 @@ class Program
             }
             mesajValidare += ' ';
             // anul nașterii
-            
+            if (mCnp.Length > 2)
+            {
+                if ( mCnp.All(Char.IsNumber) )
+                {
+                    if(mCnp[0] == '1' || mCnp[0] == '2'){
+                        mesajValidare += "19" + mCnp.Substring(1,2);
+                    }else if (mCnp[0] == '3' || mCnp[0] == '4')
+                    {
+                        mesajValidare += "18" + mCnp.Substring(1,2);
+                    }else if (mCnp[0] == '5' || mCnp[0] == '6')
+                    {
+                        if ( int.Parse("20" + mCnp.Substring(1,2)) <= DateTime.Now.Year )
+                        {
+                            mesajValidare += "20" + mCnp.Substring(1,2);
+                        }else{
+                            mesajValidare += "{?an}";
+                        }
+                    }else{
+                        mesajValidare += "{?an}";
+                    }
+                }else{
+                    mesajValidare += "{an?}";
+                }
+            }else{
+                mesajValidare += "{an?}";
+            }
         }
          
         
